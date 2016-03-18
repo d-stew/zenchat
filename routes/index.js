6cf6d5215208b6140724384a3af4a9a42d1ac7dd
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-03-18 16:18:38 -0600

    Add template and form for ZenChat

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/rooms', function(req, res, next) {
+  // Go to views directory
+  // Find a file named 'room'
+  // Run in through the template engine (handlbars)
+  // Stick it in the layout
+  // Send it back to the browser
+  res.render('room', {
+    strawberry: req.body.roomName
+  });
+  // res.send(req.body.roomName.toUpperCase())
+});
+
 module.exports = router;
